f885620ecfd5655be7f82ad4bf2a261fb47735b4
David <djknit@gmail.com>
2019-09-04 17:03:48 -0500

    updated modal skeleton

--- client/src/components/enter-bills-view/modal-skeleton/index.js
+++ client/src/components/enter-bills-view/modal-skeleton/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
 
 function ModalSkeleton({
+  title,
   isActive,
-  closeModal
+  closeModal,
+  children,
+  hasSuccess,
+  hasDanger,
+  cancel,
+  footerContent
 }) {
+
   return (
     <div className={`modal${isActive ? ' is-active' : ''}`}>
       <div className="modal-background"></div>
       <div className="modal-card">
         <header className="modal-card-head">
-          <p className="modal-card-title">Modal title</p>
+          <p className="modal-card-title">{title}</p>
           <button className="delete" aria-label="close" onClick={closeModal}></button>
         </header>
         <section className="modal-card-body">
-          content....
+          {children}
         </section>
         <footer className="modal-card-foot">
           <button className="button is-success">Save changes</button>
